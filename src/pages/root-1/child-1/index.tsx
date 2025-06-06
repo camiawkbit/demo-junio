2684916ce393121139635c41e079dd061f12b5fc
Test Demo <Test@Demo.com>
2025-06-06 19:16:37 +0000

    Update site

--- src/pages/root-1/child-1/index.tsx
+++ src/pages/root-1/child-1/index.tsx
@@ -10,8 +10,8 @@ export default function Page() {
       <Hero
         t1={
           <>
-            Before they sold out <br className="hidden lg:inline-block" />{" "}
-            readymade gluten
+            Before they sold out <br />
+            readymade gluten!
           </>
         }
         t2="Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray."
@@ -19,9 +19,9 @@ export default function Page() {
         t4="Button"
         t5={
           <Image
-            src="/720x600"
-            width={720}
-            height={600}
+            src="/Screenshot 2025-05-30 at 16.07.49.png"
+            width={870}
+            height={148}
             alt="hero"
             className="object-cover object-center rounded"
           />
